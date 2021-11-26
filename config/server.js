b116c2cf4973d5d03b29d3af49de72e20c34c008
Solitaire <samrandaofficial@gmail.com>
2021-11-26 13:25:14 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6c25b5cc529869bd83c79b68b7bea666'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb4f4830-e201-4ac9-8bfc-e774f9ad72e4'
+};
